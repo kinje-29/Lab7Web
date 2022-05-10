447dd5be4b40a681767e0084fb41c1441c9160ef
kinje-29 <fajar.vds@gmail.com>
2022-05-10 08:53:19 +0700

    Gambar

---
+++ README.md
@@ -0,0 +1,31 @@
+# Lab7Web
+
+## Belajar PHP
+
+![1.png](Gambar/1.png)
+![2.png](Gambar/2.png)
+![3.png](Gambar/3.png)
+![4.png](Gambar/4.png)
+![5.png](Gambar/5.png)
+![6.png](Gambar/6.png)
+![7.png](Gambar/7.png)
+![8.png](Gambar/8.png)
+![9.png](Gambar/9.png)
+![10.png](Gambar/10.png)
+![11.png](Gambar/11.png)
+![12.png](Gambar/12.png)
+![13.png](Gambar/13.png)
+![14.png](Gambar/14.png)
+![15.png](Gambar/15.png)
+![16.png](Gambar/16.png)
+
+## Tugas
+Pertanyaan dan Tugas
+Buatlah program PHP sederhana dengan menggunakan form input yang menampilkan nama, tanggal lahir dan pekerjaan. Kemudian tampilkan outputnya dengan menghitung umur berdasarkan inputan tanggal lahir. Dan pilihan pekerjaan dengan gaji yang berbeda-beda sesuai pilihan pekerjaan.
+
+![HasilTUgas.png](Gambar/HasilTUgas.png)
+![HasilTUgas1.png](Gambar/HasilTUgas1.png)
+![Tugas1.png](Gambar/Tugas1.png)
+![Tugas2.png](Gambar/Tugas2.png)
+![Tugas3.png](Gambar/Tugas3.png)
+![Tugas4.png](Gambar/Tugas4.png)
---
+++ tugas.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>PHP Dasar</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  <div class="bio">
+    <h2 class="biodata">Biodata</h2>
+    <hr>
+    <form action="" method="POST">
+      <label for="">Nama</label>
+      <input class="nama" type="text" name="nama" id="" placeholder="Masukan Nama....">
+      <br>
+      <label for="">tanggal lahir</label>
+      <input class="nama" type="date" name="Tanggal" value="">
+      <br>
+      <label for="job">Pekerjaan</label>
+      <select class="nama" name="job" id="job">
+        <option value="pilih">Pilih...</option>
+        <option value="programer">Programmer</option>
+        <option value="designer">Designer</option>
+        <option value="manager">Manager</option>
+        <option value="buruh">Buruh</option>
+      </select>
+      <br>
+      <input type="submit" class="kirim" name="submit" value="Submit">
+    </form>
+
+    <div class="output">
+      <?php
+      // Output Nama
+      echo 'nama saya ' . $_POST['nama'];
+      echo '<br>';
+
+      // Output tanggal lahir
+      $tanggal_lahir = new DateTime($_POST['Tanggal']);
+      $sekarang = new DateTime("today");
+      if ($tanggal_lahir > $sekarang) {
+        $thn = "0";
+        $bln = "0";
+        $tgl = "0";
+      }
+
+      $thn = $sekarang->diff($tanggal_lahir)->y;
+      echo "Umur " . $thn;
+      echo '<br>';
+
+      // Output pekerjaan
+      echo 'pekerjaan  ' . $_POST['job'];
+      echo '<br>';
+      if ($_POST['job'] == 'programer') {
+        echo 'gaji Rp. 8.000.000 Juta';
+      } elseif ($_POST['job'] == 'designer') {
+        echo 'gaji Rp. 6.000.000 juta';
+      } elseif ($_POST['job'] == 'manager') {
+        echo 'gaji Rp. 5.000.000 juta';
+      } else if ($_POST['job'] == 'buruh') {
+        echo 'Gaji Rp. 3.000.000 Juta';
+      } else {
+        echo 'Tidak Memiliki pekerjaan ';
+      }
+      ?>
+    </div>
+  </div>
+</body>
+
+</html>
